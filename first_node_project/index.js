e7e3ffed1fda4cbd5a0ae00197cc81c4dc35d87a
harshbhar0629 <harshbhar.0629@gmail.com>
2024-06-08 23:20:02 +0530

    Learn about axuis

--- first_node_project/index.js
+++ first_node_project/index.js
@@ -1,6 +1,15 @@
 const axios = require('axios').default;
 // console.log(axios);
-axios.get('https://www.omdbapi.com/?i=tt3896198&apikey=dec4ad0e')
-    .then((res) => {
-        console.log(res);
-    });
+// axios.get('https://www.omdbapi.com/?t=harry&apikey=dec4ad0e')
+//     .then((res) => {
+//         console.log(res.data);
+//     });
+
+async function fetch() {
+    const response = await axios.get(
+        "https://www.omdbapi.com/?t=harry&apikey=dec4ad0e"
+    );
+
+    console.log(response.data)
+}
+fetch();
